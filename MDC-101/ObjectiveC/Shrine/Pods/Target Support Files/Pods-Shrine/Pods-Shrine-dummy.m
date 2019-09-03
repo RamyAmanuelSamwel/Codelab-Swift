0bf7ada8cd3469ca90c71be42e1e5bd072e19ce9
RamyAmanuelSamwel <remenwel66@hotmail.com>
2019-09-03 16:30:12 +0200

    Reomving Objective-C Folders

--- MDC-101/ObjectiveC/Shrine/Pods/Target Support Files/Pods-Shrine/Pods-Shrine-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shrine : NSObject
-@end
-@implementation PodsDummy_Pods_Shrine
-@end
--- MDC-111/ObjectiveC/Complete/Pods/Target Support Files/Pods-MDC-111/Pods-MDC-111-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MDC_111 : NSObject
-@end
-@implementation PodsDummy_Pods_MDC_111
-@end
